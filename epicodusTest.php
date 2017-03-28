6f5ed6d2360f73f5072a944d6b02d56adb212dcf
milleraundra <aundra.miller@zeppidy.com>
2017-03-28 11:15:10 -0700

    create html files and form for madlib game

---
+++ epicodusTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aundramiller
+ * Date: 3/27/17
+ * Time: 2:42 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Epicodus Test</title>
+    </head>
+    <body>
+        <h1>Toothless? I could have sworn you had teeth.</h1>
+    </body>
+</html>
---
+++ madlibResult.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aundramiller
+ * Date: 3/28/17
+ * Time: 11:00 AM
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<p>American children are fascinated by <?php echo $_GET['adjective']?>.</p>
+<h1>Adjective: </h1>
+<h1>pluralNoun1: <?php echo $_GET['pluralNoun1']?></h1>
+<h1>pluralNoun2: <?php echo $_GET['pluralNoun2']?></h1>
+<h1>sillyWord: <?php echo $_GET['sillyWord']?></h1>
+<h1>liquid: <?php echo $_GET['liquid']?></h1>
+
+</html>
