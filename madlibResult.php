0c4b546b613540998df9ce5557b79cd9915d0c21
milleraundra <aundra.miller@zeppidy.com>
2017-03-28 11:20:56 -0700

    update php variable

--- madlibResult.php
+++ madlibResult.php
@@ -11,7 +11,7 @@
 <!DOCTYPE html>
 <html>
 
-<p>American children are fascinated by <?php echo $_GET['adjective']?> stuff -- like stories that scare the <?php echo $_GET['pluralNoun1']?> off them or make their <?php echo $_GET['pluralNoun1']?> stand on end.</p>
+<p>American children are fascinated by <?php echo $_GET['adjective']?> stuff -- like stories that scare the <?php echo $_GET['pluralNoun1']?> off them or make their <?php echo $_GET['pluralNoun2']?> stand on end.</p>
 
 <p>Scientists say this is because being frightened causes the <?php echo $_GET['sillyWord']?> gland to function and puts <?php echo $_GET['liquid']?> into their blood.</p>
 </html>
